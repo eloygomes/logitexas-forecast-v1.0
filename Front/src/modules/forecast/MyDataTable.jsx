e0782a1c3233747b5ee82864b68a4b2a510727a1
Eloy Gomes <eloy.gomes@icloud.com>
2025-04-19 10:31:54 -0300

    0.5.14.0
    - pre deploy of freeze columns/rows

--- Front/src/modules/forecast/MyDataTable.jsx
+++ Front/src/modules/forecast/MyDataTable.jsx
@@ -4,61 +4,7 @@ import {
   showSuccess,
   showError,
 } from "../../components/sooner/SonnerToastProvider.jsx";
-
-// Configurações padrão para as colunas (por índice) - 47 itens já definidos
-const defaultColumnStyles = [
-  { width: "80px", headerBg: "#f0f0f0", label: "id" }, // 1
-  { width: "200px", headerBg: "#e0e0e0", label: "Cliente" }, // 2
-  { width: "120px", headerBg: "#d1d1d1", label: "NAM" }, // 3
-  { width: "150px", headerBg: "#d2d2d2", label: "MANAGER" }, // 4
-  { width: "130px", headerBg: "#d3d3d3", label: "Part Number" }, // 5
-  { width: "500px", headerBg: "#d4d4d4", label: "Descrição do produto" }, // 6
-  { width: "300px", headerBg: "#d5d5d5", label: "Runrate NPI" }, // 7
-  { width: "300px", headerBg: "#d6d6d6", label: "Product Group" }, // 8
-  { width: "300px", headerBg: "#d7d7d7", label: "CHAVE" }, // 9
-  {
-    width: "100px",
-    headerBg: "#EE9B00",
-    label: "Estoque Cliente (PÇS)",
-  },
-  { width: "100px", headerBg: "#EE9B00", label: "Estoque Projetado" },
-  { width: "100px", headerBg: "#EE9B00", label: "Estoque Ajustado" },
-  { width: "100px", headerBg: "#EE9B00", label: "WOH" },
-  { width: "100px", headerBg: "#EE9B00", label: "Abril" },
-  { width: "100px", headerBg: "#EE9B00", label: "Maio" },
-  { width: "100px", headerBg: "#EE9B00", label: "Junho" },
-  { width: "100px", headerBg: "#EE9B00", label: "Forecast Q1" },
-  { width: "100px", headerBg: "#EE9B00", label: "Total Q1 Ano Anterior" },
-  { width: "100px", headerBg: "#EE9B00", label: "Forecast & Evento Q1" },
-  { width: "100px", headerBg: "#EE9B00", label: "Variação YoY" },
-  { width: "100px", headerBg: "#0A9396", label: "Estoque_Projetado" },
-  { width: "100px", headerBg: "#0A9396", label: "WOH" },
-  { width: "100px", headerBg: "#0A9396", label: "Julho" },
-  { width: "100px", headerBg: "#0A9396", label: "Agosto" },
-  { width: "100px", headerBg: "#0A9396", label: "Setembro" },
-  { width: "100px", headerBg: "#0A9396", label: "Forecast_Q2" },
-  { width: "100px", headerBg: "#0A9396", label: "Total Q2 Ano Anterior" },
-  { width: "100px", headerBg: "#0A9396", label: "Forecast Q2 Evento Q2" },
-  { width: "100px", headerBg: "#0A9396", label: "Variação_YoY" },
-  { width: "100px", headerBg: "#94D2BD", label: "Estoque_Projetado" },
-  { width: "100px", headerBg: "#94D2BD", label: "WOH" },
-  { width: "100px", headerBg: "#94D2BD", label: "Outubro" },
-  { width: "100px", headerBg: "#94D2BD", label: "Novembro" },
-  { width: "100px", headerBg: "#94D2BD", label: "Dezembro" },
-  { width: "100px", headerBg: "#94D2BD", label: "Forecast Q3" },
-  { width: "100px", headerBg: "#94D2BD", label: "Total Q3 Ano Anterior" },
-  { width: "100px", headerBg: "#94D2BD", label: "Forecast Q3 Evento Q3" },
-  { width: "100px", headerBg: "#94D2BD", label: "Variação YoY" },
-  { width: "100px", headerBg: "#E9D8A6", label: "Estoque Projetado" },
-  { width: "100px", headerBg: "#E9D8A6", label: "WOH" },
-  { width: "100px", headerBg: "#E9D8A6", label: "Janeiro" },
-  { width: "100px", headerBg: "#E9D8A6", label: "Fevereiro" },
-  { width: "100px", headerBg: "#E9D8A6", label: "Março" },
-  { width: "100px", headerBg: "#E9D8A6", label: "Forecast Q4" },
-  { width: "100px", headerBg: "#E9D8A6", label: "Total Q4 ano anterior" },
-  { width: "100px", headerBg: "#E9D8A6", label: "Forecast Q4 Evento Q4" },
-  { width: "100px", headerBg: "#E9D8A6", label: "Variação YoY" },
-];
+import dColumnStyles from "./header/dColumnStyles.jsx";
 
 export function MyDataTable({
   columns,
@@ -80,19 +26,19 @@ export function MyDataTable({
     ...col,
     width:
       col.width ||
-      (defaultColumnStyles[index] && defaultColumnStyles[index].width) ||
+      (dColumnStyles[index] && dColumnStyles[index].width) ||
       "150px",
 
-    // b) Use headerBg from either the column itself or fallback to defaultColumnStyles
+    // b) Use headerBg from either the column itself or fallback to dColumnStyles
     headerBg:
       col.headerBg ||
-      (defaultColumnStyles[index] && defaultColumnStyles[index].headerBg) ||
+      (dColumnStyles[index] && dColumnStyles[index].headerBg) ||
       "gray",
 
     // c) Here we inject a "label" that we can use as a fallback if col.header doesn’t exist
     label:
       col.label ||
-      (defaultColumnStyles[index] && defaultColumnStyles[index].label) ||
+      (dColumnStyles[index] && dColumnStyles[index].label) ||
       (typeof col.header === "string" ? col.header : "sem nome"),
   }));
 
